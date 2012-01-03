ce10cbc89148ec522bb989bf0213c4defa286dac
John <je.llyb..ean.s...and.jo..h.n+github@gmail.com>
2012-01-02 22:14:18 -0800

    Fix: demos/omegle-cli-client broke at some point (TBH It shouldn't have worked in the first place, no thread synchronization). I've switched it over to using nonblocking stdin IO with read(), although this means another spinlock. (select()ing on an omegleConnection is in the works...)

--- demos/omegle-cli-client.cpp
+++ demos/omegle-cli-client.cpp
@@ -1,63 +1,95 @@
 #include <iostream>
 #include <string>
-#include <thread>
+#include <cassert>
 #include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <cerrno>
 
 #include "Omegle.h"
 
 // This demo is in the public domain and free to be used for whatever purpose.
 
-// See basic-chatbot.cpp for a well-commented introductory use of the more basic API. This demo demos the more advanced API and handling all events. 
+// See basic-chatbot.cpp for a well-commented introductory use of the more basic API.
 
-void inputThreadMain(Omegle::Connection* omegleConnection)
-{
-  while(true)
-  {
-    std::string message;
-      
-    char c = std::cin.get();
-    message += c;
-
-    omegleConnection->SendTyping();
-
-    while((c = std::cin.get()) != '\n')
-    {
-      message += c;
-    }
-
-    if(message == "\\quit")
-    {
-      std::cerr << "(You have disconnected)" << std::endl;
-      omegleConnection->Disconnect();
-      exit(0);
-    }
-
-    omegleConnection->SendMessage(message);
-    std::cout << "You: " << message << std::endl; 
-  }
-}
+// This ended up quite a bit longer than it should be because of the difficulties in asynchronous terminal input/output.
 
 int main()
 {
-  Omegle::Connection omegleConnection;
-  std::cerr << "You're now chatting with a random stranger. Say hi!" << std::endl;
-
-  std::thread inputThread(inputThreadMain, &omegleConnection);
-
   try
   {
+    Omegle::Connection omegleConnection;
+    std::cerr << "You're now chatting with a random stranger. Say hi!" << std::endl;
+
+    std::string inputBuffer;
+
+    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+
     while(true)
     {
+      // I should just select() on stdin and the omegleConnection instead of relying on two nonblocking calls, but this is easier said than done because the raw FD is not exposed by libomegle.
+      usleep(50000); //Prevent spinlock from eating CPU
+
+      // Handle input
+      char input[256];
+
+      ssize_t stdinLenRead = read(STDIN_FILENO, input, 256);
+      if(stdinLenRead > 0)
+      {
+        if(inputBuffer == "")
+        {
+          omegleConnection.SendTyping();
+        }
+          
+        inputBuffer.append(input, stdinLenRead);
+
+        unsigned int newlinePos;
+        while((newlinePos = inputBuffer.find('\n')) != std::string::npos) //While complete messages have been entered by the user
+        {
+          std::string message = inputBuffer.substr(0, newlinePos); //Split the message from excess input
+          inputBuffer = inputBuffer.substr(newlinePos+1, inputBuffer.length()); //Leave whatever excess you have typed in the input buffer to be handled next iteration
+
+          if(message == "\\quit") //CTRL+D works too
+          {
+            std::cerr << "(You have disconnected)" << std::endl;
+            omegleConnection.Disconnect();
+            exit(0);
+          }
+
+          omegleConnection.SendMessage(message);
+          std::cout << "You: " << message << std::endl;
+
+          if(!inputBuffer.empty())
+          {
+            omegleConnection.SendTyping();
+          }
+        }
+      }
+      else if(stdinLenRead < 0)
+      {
+        assert(errno == EAGAIN);
+      }
+      else //stdinLenRead = 0 //EOF //CTRL+D
+      {
+        std::cerr << "(You have disconnected)" << std::endl;
+        omegleConnection.Disconnect();
+        exit(0);
+      }
+
+      // Handle packets
       Omegle::PacketId packetId;
       Omegle::Packet* packet;
 
-      omegleConnection.PollEvent(&packetId, &packet, Omegle::BLOCKING);
+      omegleConnection.PollEvent(&packetId, &packet, Omegle::NONBLOCKING);
 
       if(packetId == Omegle::PID_STRANGERMESSAGE)
       {
         std::cout << "Stranger: " << static_cast<Omegle::StrangerMessagePacket*>(packet)->message << std::endl;
       }
-      else if(packetId == Omegle::PID_TYPING)
+      else if(packetId == Omegle::PID_STARTTYPING)
       {
         std::cerr << "(Stranger is typing)" << std::endl;
       }
@@ -72,9 +104,9 @@ int main()
       }
     }
   }
-  catch(Omegle::SocketError)
+  catch(Omegle::NetworkError& e)
   {
-    std::cerr << "Network failure." << std::endl;
+    std::cerr << "(network failure: " << e.errnoMessage << ")" << std::endl;
     exit(1);
   }
   catch(Omegle::CaptchaError)
